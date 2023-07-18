e0708810f24e83cb483cb8ca943613921cb938a0
Amsav23 <130859471+Amsav23@users.noreply.github.com>
2023-07-18 17:44:16 -0400

    testing useEffect

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,10 @@ import './Footer.css'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="footer-container">
+        Made with love by mimo
+        <button className="contact-btn">Contact Us</button>
+    </div>
   )
 }
 
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './Homepage.css'
 
 function Homepage() {
+    //This page shows All products when it loads
+    //https://fakestoreapi.com/products
+
+    useEffect(
+        ()=>{
+            console.log('homepage loaded')
+
+        }, [] //run once when the page loads
+    )
+
   return (
-    <div>Homepage</div>
+    <div className="homepage-container">
+        <div className="products-container">
+            Products go here
+        </div>
+    </div>
   )
 }
 
