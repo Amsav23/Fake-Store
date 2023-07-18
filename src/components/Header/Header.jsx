c0634b74b619207d31181cf7a71562fad27241ff
Amsav23 <130859471+Amsav23@users.noreply.github.com>
2023-07-18 16:33:13 -0400

    Initial set up of Fake Store

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="header-container">
+        <a href='#'>Fake Store</a>
+        <a href='#'>Shopping Cart Icon</a>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Menu.css'
+
+function Menu() {
+  return (
+    <div className="menu-container">
+        <a href='#'>All</a>
+        <a href='#'>Electronics</a>
+        <a href='#'>Jewelery</a>
+        <a href='#'>Men's Clothing</a>
+        <a href='#'>Women's Clothing</a>
+    </div>
+  )
+}
+
+export default Menu
